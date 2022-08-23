a77d9881a871361d4f92b79a5342fbfc558929cd
Fabio-Vini <88862357+Fabio-Vini@users.noreply.github.com>
2022-08-23 17:52:30 -0300

    Update C# (exercicio 4).cs

--- Exercicios C#/Prática/C# (exercicio 4).cs
+++ Exercicios C#/Prática/C# (exercicio 4).cs
@@ -10,13 +10,18 @@ public class Class1
             double valor;
             double desconto;
             double salario;
+		
             Console.WriteLine("Digite a quantidade de aulas dadas");
             aulas = double.Parse(Console.ReadLine());
+		
             Console.WriteLine("Digite o valor da hora aula");
             valor = double.Parse(Console.ReadLine());
+		
             Console.WriteLine("Digite o percentual de desconto do INSS");
             desconto = double.Parse(Console.ReadLine());
+		
             salario = (aulas * valor) * (1 - desconto / 100);
+		
             Console.WriteLine("Seu salario é: " + salario);
         }
 
